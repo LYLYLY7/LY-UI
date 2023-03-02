34fe84cbc23b8108bf720cd692e9c93bc092d165
LYLYLY <lylyly72020@163.com>
2023-03-02 20:11:56 +0800

    将main.ts的router封装到router.ts，然后导出，在main.ts引用。从而保证App.vue能够访问router

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,8 @@
 import {createApp} from 'vue';
 import './style.scss';
 import App from './App.vue';
-import {createWebHashHistory, createRouter} from 'Vue-router';
-import Home from './views/Home.vue';
-import Doc from './views/Doc.vue';
-import Switch from './components/Switch.vue';
-import Button from './components/Button.vue';
-import Dialog from './components/Dialog.vue';
-import Tab from './components/Tab.vue';
+import {router} from './router';
 
-
-const history = createWebHashHistory();
-const router = createRouter({
-  history: history,
-  routes: [
-    {path: '/', component: Home},
-    {
-      path: '/doc', component: Doc,
-      children: [
-        {path: 'switch', component: Switch},
-        {path: 'button', component: Button},
-        {path: 'dialog', component: Dialog},
-        {path: 'tab', component: Tab}
-      ]
-    }
-
-  ]
-});
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
---
+++ src/router.ts
@@ -0,0 +1,27 @@
+import {createRouter, createWebHashHistory} from 'vue-router';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
+import Switch from './components/Switch.vue';
+import Button from './components/Button.vue';
+import Dialog from './components/Dialog.vue';
+import Tab from './components/Tab.vue';
+
+const history = createWebHashHistory();
+export const router = createRouter({
+  history: history,
+  routes: [
+    {path: '/', component: Home},
+    {
+      path: '/doc', component: Doc,
+      children: [
+        {path: 'switch', component: Switch},
+        {path: 'button', component: Button},
+        {path: 'dialog', component: Dialog},
+        {path: 'tab', component: Tab}
+      ]
+    }
+  ]
+});
+router.afterEach(() => {
+  console.log('路由切换了')
+});
